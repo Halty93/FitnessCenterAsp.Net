93328bd643b28f626bc8d2d2e4341ba6868af865
Halty93 <david.hal@seznam.cz>
2017-10-10 18:10:11 +0200

    Data Acces Ini, Activity Ini, layout

---
+++ DataAccess/Model/Activity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Model
+{
+    public class Activity
+    {
+        private int Id { get; set; }
+        private string Name { get; set; }
+        string Description { get; set; }
+        int Difficultness { get; set; }
+
+    }
+}
---
+++ DataAccess1/Model/Activity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Model
+{
+    public class Activity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Difficultness { get; set; }
+        //pridat trenera jako zakladatele kurzu
+    }
+}
